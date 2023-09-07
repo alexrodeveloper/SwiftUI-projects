3c16e4774dc4bd51b3966371b14169313402ca5c
Alex Kimyung Ro <121688621+alexrodeveloper@users.noreply.github.com>
2023-09-07 15:50:04 -0500

    Add files via upload

---
+++ 2. GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Alex Kimyung Ro on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
