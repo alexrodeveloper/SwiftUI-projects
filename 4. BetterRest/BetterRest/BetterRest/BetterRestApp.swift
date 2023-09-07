42f080db3c48de4c3525a57ee8175850ca67a0a8
Alex Kimyung Ro <121688621+alexrodeveloper@users.noreply.github.com>
2023-09-07 17:22:04 -0500

    Add files via upload
    first commit

---
+++ 4. BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alex Kimyung Ro on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
