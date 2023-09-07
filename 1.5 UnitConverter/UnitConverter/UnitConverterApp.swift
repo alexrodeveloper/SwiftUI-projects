2a9e71f380379b5836659e7c101f3222c90ef7cf
Alex Kimyung Ro <121688621+alexrodeveloper@users.noreply.github.com>
2023-09-07 15:50:24 -0500

    Add files via upload

---
+++ 1.5 UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Alex Kimyung Ro on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
